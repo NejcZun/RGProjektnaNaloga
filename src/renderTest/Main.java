bc0d7ff0ef41e5ec1cb4b9edff411e25620e61e8
NejcZun <46348765+NejcZun@users.noreply.github.com>
2019-10-11 23:09:31 +0200

    Added Terrain Generation and OBJ Model loading Camera is fucker (WIP)

--- src/renderTest/Main.java
+++ src/renderTest/Main.java
@@ -1,48 +1,53 @@
 package renderTest;
 
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
+import models.RawModel;
+import models.TexturedModel;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
-import renderEngine.Renderer;
+import renderEngine.MasterRenderer;
+import renderEngine.OBJLoader;
 import shaders.StaticShader;
+import terrains.Terrain;
+import textures.ModelTexture;
 
 public class Main {
-
-
-
-
     public static void main(String[] args){
 
-
         DisplayManager.createDisplay();
-
         Loader loader = new Loader();
-        Renderer renderer = new Renderer();
-        StaticShader shader = new StaticShader();
 
-        float[] vertices = {
-                -0.5f, 0.5f, 0f,
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f,
-        };
 
-        int[] indices = {
-                0,1,3,
-                3,1,2
-        };
+        RawModel model = OBJLoader.loadObjModel("tree", loader);
 
-        RawModel model = loader.loaderToVAO(vertices, indices);
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("tree")));
+        ModelTexture texture = staticModel.getTexture();
+
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-25), 0, 0, 0,1);
+        Light light = new Light(new Vector3f(3000, 2000, 2000), new Vector3f(1,1,1));
+
+        Terrain terrain = new Terrain(0,0,loader, new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain2 = new Terrain(1,0,loader, new ModelTexture(loader.loadTexture("grass")));
+
+        Camera camera = new Camera();
+        MasterRenderer renderer = new MasterRenderer();
 
         while(!Display.isCloseRequested()){
-            renderer.prepare();
-            shader.start();
-            renderer.render(model);
-            shader.stop();
+            entity.increaseRotation(0,1,0);
+            camera.move();
+
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
+
+            renderer.processEntity(entity);
+            renderer.render(light,camera);
             DisplayManager.updateDisplay();
         }
-        shader.cleanUp();
+
         loader.cleanUp();
         DisplayManager.closeDisplay();
 
