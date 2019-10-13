2f4efd87181a32b18dd6e9edbdaa8e69b8ff961c
NejcZun <46348765+NejcZun@users.noreply.github.com>
2019-10-13 11:03:49 +0200

    Added Day/Night Cycle

--- src/renderEngine/MasterRenderer.java
+++ src/renderEngine/MasterRenderer.java
@@ -74,7 +74,7 @@ public class MasterRenderer {
 		terrainShader.loadViewMatrix(camera);
 		terrainRenderer.render(terrains);
 		terrainShader.stop();
-		skyboxRenderer.render(camera);
+		skyboxRenderer.render(camera, RED, GREEN, BLUE);
 		terrains.clear();
 		entities.clear();
 	}
--- src/skybox/SkyboxShader.java
+++ src/skybox/SkyboxShader.java
@@ -4,6 +4,8 @@ import org.lwjgl.util.vector.Matrix4f;
 
 import entities.Camera;
 
+import org.lwjgl.util.vector.Vector3f;
+import renderEngine.DisplayManager;
 import shaders.ShaderProgram;
 import toolbox.Maths;
 
@@ -12,8 +14,16 @@ public class SkyboxShader extends ShaderProgram{
     private static final String VERTEX_FILE = "src/skybox/skyboxVertexShader.txt";
     private static final String FRAGMENT_FILE = "src/skybox/skyboxFragmentShader.txt";
 
+    private static final float ROTATE_SPEED = 1f;
+
     private int location_projectionMatrix;
     private int location_viewMatrix;
+    private int location_fogColour;
+    private int location_CubeMap;
+    private int location_CubeMap2;
+    private int location_blendFactor;
+
+    private float rotation = 0;
 
     public SkyboxShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -28,13 +38,32 @@ public class SkyboxShader extends ShaderProgram{
         matrix.m30 = 0;
         matrix.m31 = 0;
         matrix.m32 = 0;
+        rotation += ROTATE_SPEED * DisplayManager.getFrameTimeSeconds();
+        Matrix4f.rotate((float)Math.toRadians(rotation), new Vector3f(0, 1, 0), matrix, matrix);
         super.loadMatrix(location_viewMatrix, matrix);
+    }
+
+    public void loadFogColour(float r, float g, float b){
+        super.loadVector(location_fogColour, new Vector3f(r, g, b));
+    }
+
+    public void loadBlendFactor(float blend){
+        super.loadFloat(location_blendFactor, blend);
+    }
+    public void connectTextureUnits(){
+        super.loadInt(location_CubeMap, 0);
+        super.loadInt(location_CubeMap2, 1);
     }
 
     @Override
     protected void getAllUniformLocations() {
         location_projectionMatrix = super.getUniformLocation("projectionMatrix");
         location_viewMatrix = super.getUniformLocation("viewMatrix");
+        location_fogColour = super.getUniformLocation("fogColour");
+        location_blendFactor = super.getUniformLocation("blendFactor");
+        location_CubeMap = super.getUniformLocation("cubeMap");
+        location_CubeMap2 = super.getUniformLocation("cubeMap2");
+
     }
 
     @Override
