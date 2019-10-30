60355eb160fb0e245b9fd59222f3e00bac1d71db
NejcZun <46348765+NejcZun@users.noreply.github.com>
2019-10-30 15:22:00 +0100

    New Player Texture

--- src/renderTest/Main.java
+++ src/renderTest/Main.java
@@ -48,6 +48,8 @@ public class Main {
         Loader loader = new Loader();
         TextMaster.init(loader);
 
+        //MUSIC
+
         MusicMixer backgroundMusic = new MusicMixer("background.ogg");
         backgroundMusic.play();
 
